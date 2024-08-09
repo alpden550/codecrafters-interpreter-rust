34e43206400fb1a7b47bc482e54f3b4e1de469c9
Denis Novikov <alpden550@gmail.com>
2024-08-09 15:14:52 +0300

    parse not allowed symbols

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crate::tokens::token_type::TokenType;
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,7 +27,10 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
-                parse_file_content(file_contents);
+                let exit_code = parse_file_content(&file_contents);
+                if exit_code != 0 {
+                    exit(exit_code);
+                }
             } else {
                 println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
@@ -38,22 +42,35 @@ fn main() {
     }
 }
 
-fn parse_file_content(file_contents: String) {
-    for char in file_contents.chars() {
-        match char {
-            '(' => println!("{} {char} null", TokenType::LeftParen),
-            ')' => println!("{} {char} null", TokenType::RightParen),
-            '{' => println!("{} {char} null", TokenType::LeftBrace),
-            '}' => println!("{} {char} null", TokenType::RightBrace),
-            ',' => println!("{} {char} null", TokenType::Comma),
-            '.' => println!("{} {char} null", TokenType::Dot),
-            '-' => println!("{} {char} null", TokenType::Minus),
-            '+' => println!("{} {char} null", TokenType::Plus),
-            ';' => println!("{} {char} null", TokenType::Semicolon),
-            '*' => println!("{} {char} null", TokenType::Star),
-            '/' => println!("{} {char} null", TokenType::Slash),
-            _ => {}
+fn parse_file_content(file_contents: &String) -> i32 {
+    let mut exit_code = 0;
+    let lines = file_contents.lines();
+
+    for (line_number, line) in lines.enumerate() {
+        for char in line.chars() {
+            match char {
+                '(' => println!("{} {char} null", TokenType::LeftParen),
+                ')' => println!("{} {char} null", TokenType::RightParen),
+                '{' => println!("{} {char} null", TokenType::LeftBrace),
+                '}' => println!("{} {char} null", TokenType::RightBrace),
+                ',' => println!("{} {char} null", TokenType::Comma),
+                '.' => println!("{} {char} null", TokenType::Dot),
+                '-' => println!("{} {char} null", TokenType::Minus),
+                '+' => println!("{} {char} null", TokenType::Plus),
+                ';' => println!("{} {char} null", TokenType::Semicolon),
+                '*' => println!("{} {char} null", TokenType::Star),
+                '/' => println!("{} {char} null", TokenType::Slash),
+                _ => {
+                    eprintln!(
+                        "[line {}] Error: Unexpected character: {char}",
+                        line_number + 1
+                    );
+                    exit_code = 65;
+                }
+            }
         }
     }
+
     println!("{}  null", TokenType::Eof);
+    exit_code
 }
