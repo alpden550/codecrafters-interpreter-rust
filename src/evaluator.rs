680f8e08370c0c797117309c0b83d3a2bbb80c06
Denis Novikov <alpden550@gmail.com>
2024-08-13 12:56:15 +0300

    evaluate exp

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,5 +1,49 @@
 use crate::tokens::token::Token;
+use crate::tokens::token_type::TokenType;
+use std::fmt::{Display, Formatter};
 
-pub fn evaluate(token: &Token) -> String {
-    token.name.clone()
+pub enum Value {
+    Nil,
+    Bool(bool),
+    String(String),
+    Number(f64),
+    None,
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Nil => write!(f, "nil"),
+            Self::Bool(b) => write!(f, "{b}"),
+            Self::String(s) => write!(f, "{s}"),
+            Self::Number(n) => write!(f, "{n}"),
+            Self::None => write!(f, ""),
+        }
+    }
+}
+
+impl Value {
+    fn str_to_bool(s: &str) -> Result<bool, &str> {
+        match s.to_lowercase().as_str() {
+            "true" => Ok(true),
+            "false" => Ok(false),
+            _ => Err("Invalid input"),
+        }
+    }
+}
+
+pub fn evaluate(token: &Token) -> Value {
+    let name = token.name.as_str();
+    let value = token.value.clone();
+
+    match token.token_type {
+        TokenType::Nil => Value::Nil,
+        TokenType::False | TokenType::True => {
+            let boolean = Value::str_to_bool(name).unwrap_or(false);
+            Value::Bool(boolean)
+        }
+        TokenType::String => Value::String(value.unwrap_or("".to_string())),
+        TokenType::Number => Value::Number(value.unwrap().parse().unwrap()),
+        _ => Value::None,
+    }
 }
