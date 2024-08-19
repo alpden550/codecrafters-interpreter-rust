b2d97813e3755ecb616015643d4b087e2224233d
Denis Novikov <alpden550@gmail.com>
2024-08-19 16:09:06 +0300

    feat(parser): add parsing var and print stmts

--- src/parsers/parser.rs
+++ src/parsers/parser.rs
@@ -1,24 +1,24 @@
 use crate::errors::ExitCode;
-use crate::parsers::expressions::{Expr, Value};
+use crate::parsers::expressions::{Expr, Stmt, Value};
 use crate::tokens::token::Token;
 use crate::tokens::token_type::TokenType;
 use std::process::exit;
 
-#[allow(dead_code)]
 #[derive(Default, Debug)]
 pub struct Parser<'a> {
     tokens: &'a [Token],
     current: usize,
     pub exprs: Vec<Expr>,
+    pub stmts: Vec<Stmt>,
 }
 
-#[allow(dead_code)]
 impl<'a> Parser<'a> {
     pub fn new(tokens: &'a Vec<Token>) -> Self {
         Parser {
             tokens,
             current: 0,
             exprs: Vec::new(),
+            stmts: Vec::new(),
         }
     }
 
@@ -34,6 +34,18 @@ impl<'a> Parser<'a> {
         }
     }
 
+    pub fn parse_stmts(&mut self) {
+        while !self.is_at_end() {
+            match self.declaration() {
+                Ok(s) => self.stmts.push(s),
+                Err(e) => {
+                    eprintln!("{e}");
+                    exit(ExitCode::ExitError as i32);
+                }
+            }
+        }
+    }
+
     fn is_at_end(&self) -> bool {
         self.peek().token_type == TokenType::Eof
     }
@@ -44,6 +56,15 @@ impl<'a> Parser<'a> {
 
     fn previous(&self) -> &Token {
         &self.tokens[self.current - 1]
+    }
+
+    fn consume(&mut self, token_type: TokenType, error: String) -> Result<&Token, String> {
+        let token = self.peek();
+        if token.token_type == token_type {
+            return Ok(self.advance());
+        }
+
+        Err(format!("[line {}] {}", self.previous().line_number, error))
     }
 
     fn advance(&mut self) -> &Token {
@@ -64,7 +85,20 @@ impl<'a> Parser<'a> {
     }
 
     fn expression(&mut self) -> Result<Expr, String> {
-        self.equality()
+        self.assignment()
+    }
+
+    fn assignment(&mut self) -> Result<Expr, String> {
+        let expr = self.equality()?;
+        if self.match_tokens(&[TokenType::Equal]) {
+            let expr_value = self.assignment()?;
+            return match expr {
+                Expr::Var(t) => Ok(Expr::Assign(t, Box::new(expr_value))),
+                _ => Err("Invalid assignment target.".to_string()),
+            };
+        }
+
+        Ok(expr)
     }
 
     fn equality(&mut self) -> Result<Expr, String> {
@@ -132,6 +166,10 @@ impl<'a> Parser<'a> {
     }
 
     fn primary(&mut self) -> Result<Expr, String> {
+        if self.previous().token_type == TokenType::Identifier {
+            return Ok(Expr::Var(self.previous().clone()));
+        }
+
         let token = self.advance();
         match token.token_type {
             TokenType::Nil => Ok(Expr::Literal(Value::Nil)),
@@ -161,5 +199,68 @@ impl<'a> Parser<'a> {
                 self.previous().line_number
             )),
         }
+    }
+
+    fn declaration(&mut self) -> Result<Stmt, String> {
+        let token = self.advance();
+        if token.token_type == TokenType::Var {
+            return self.var_declaration();
+        }
+
+        self.statement()
+    }
+
+    fn var_declaration(&mut self) -> Result<Stmt, String> {
+        let token = self
+            .consume(TokenType::Identifier, "Expect variable name.".to_string())?
+            .clone();
+
+        if !(self.peek().token_type == TokenType::Equal) {
+            return Err(format!(
+                "[line {}]: expect assigning.",
+                self.previous().line_number
+            ));
+        }
+        let initializer = self.expression()?;
+        self.consume(
+            TokenType::Semicolon,
+            "Expect ';' after variable declaration.".to_string(),
+        )?;
+
+        Ok(Stmt::Variable(token, initializer))
+    }
+
+    fn statement(&mut self) -> Result<Stmt, String> {
+        let token = self.previous();
+        match token.token_type {
+            TokenType::Print => self.print_statement(),
+            _ => {
+                let value = self.expression_statement()?;
+                Ok(value)
+            }
+        }
+    }
+
+    fn print_statement(&mut self) -> Result<Stmt, String> {
+        let expr = self.expression()?;
+        self.consume(TokenType::Semicolon, "Expect ';' after value.".to_string())?;
+
+        match expr {
+            Expr::Literal(Value::String(_)) => Ok(Stmt::Print(expr)),
+            _ => Err(format!(
+                "[line {}] print statement supports only strings",
+                self.previous().line_number
+            )),
+        }
+    }
+
+    fn expression_statement(&mut self) -> Result<Stmt, String> {
+        let expr = self.expression()?;
+        self.consume(
+            TokenType::Semicolon,
+            "Expect ';' after expression.".to_string(),
+        )?;
+
+        Ok(Stmt::Expression(expr))
     }
 }
