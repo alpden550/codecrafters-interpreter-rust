ced40991fa99c4c8de9b490f8c204c415619c6c5
Denis Novikov <alpden550@gmail.com>
2024-08-19 16:08:02 +0300

    feat(expression): add var and assign

--- src/parsers/expressions.rs
+++ src/parsers/expressions.rs
@@ -1,7 +1,6 @@
 use crate::tokens::token::Token;
 use std::fmt::{Display, Formatter};
 
-#[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub enum Value {
     Nil,
@@ -26,6 +25,8 @@ pub enum Expr {
     Literal(Value),
     Unary(Token, Box<Expr>),
     Grouping(Box<Expr>),
+    Var(Token),
+    Assign(Token, Box<Expr>),
     Binary(Box<Expr>, Token, Box<Expr>),
 }
 
@@ -38,7 +39,26 @@ impl Display for Expr {
             Self::Literal(Value::Number(n)) => write!(f, "{:?}", n),
             Self::Grouping(e) => write!(f, "(group {e})"),
             Self::Unary(t, e) => write!(f, "({} {e})", t.name),
+            Self::Var(s) => write!(f, "{}", s),
             Self::Binary(l, o, r) => write!(f, "({} {l} {r})", o.name),
+            Self::Assign(t, e) => write!(f, "({} {e})", t.name),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Stmt {
+    Print(Expr),
+    Expression(Expr),
+    Variable(Token, Expr),
+}
+
+impl Display for Stmt {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Print(e) => write!(f, "PRINT {:?}", e),
+            Self::Expression(s) => write!(f, "{s}"),
+            Self::Variable(t, e) => write!(f, "{} {}", t.name, e),
         }
     }
 }
