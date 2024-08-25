c32aff7269dcd247f0616ea630c59bf1776420a9
Denis Novikov <alpden550@gmail.com>
2024-08-25 10:49:43 +0300

    refact(environment): use enclosing first

--- src/environments.rs
+++ src/environments.rs
@@ -2,8 +2,9 @@ use crate::models::values::Value;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Environment {
+    pub enclosing: Option<Box<Environment>>,
     pub values: HashMap<String, Value>,
 }
 
@@ -19,15 +20,27 @@ impl Display for Environment {
 impl Environment {
     pub fn new() -> Self {
         Environment {
+            enclosing: None,
             values: HashMap::new(),
         }
     }
 
     pub fn insert(&mut self, key: String, value: Value) {
+        if let Some(ref mut e) = self.enclosing {
+            e.insert(key, value);
+            return;
+        }
         self.values.insert(key, value);
     }
 
     pub fn get(&self, key: &str) -> Option<&Value> {
-        self.values.get(key)
+        if let Some(ref e) = self.enclosing {
+            match e.get(key) {
+                Some(v) => Some(v),
+                None => self.values.get(key),
+            }
+        } else {
+            self.values.get(key)
+        }
     }
 }
