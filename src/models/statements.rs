0a26ed253b56500fef99b3d3cebfc3978924473a
Denis Novikov <alpden550@gmail.com>
2024-08-25 15:28:54 +0300

    feat(models): add if cause for stmt

--- src/models/statements.rs
+++ src/models/statements.rs
@@ -7,16 +7,18 @@ pub enum Stmt {
     Block(Box<Vec<Stmt>>),
     Print(Expr),
     Expression(Expr),
+    If(Expr, Box<Stmt>, Option<Box<Stmt>>),
     Var(Token, Option<Expr>),
 }
 
 impl Display for Stmt {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::Block(s) => write!(f, "Block for {:?}", s),
             Self::Print(e) => write!(f, "Print {e}"),
             Self::Expression(e) => write!(f, "Expression {e}"),
+            Self::If(e, tb, eb) => write!(f, "If {} for than {:?} else {:?}", e, tb, eb),
             Self::Var(t, e) => write!(f, "Variable {t} for {:?}", e),
-            Self::Block(s) => write!(f, "Block for {:?}", s),
         }
     }
 }
