d9090855400bfd4cd26f89458418e008f51dfa0d
Denis Novikov <alpden550@gmail.com>
2024-08-27 10:04:16 +0300

    feat(statements): add while stmt

--- src/models/statements.rs
+++ src/models/statements.rs
@@ -4,21 +4,23 @@ use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Clone)]
 pub enum Stmt {
-    Block(Box<Vec<Stmt>>),
-    Print(Expr),
     Expression(Expr),
     If(Expr, Box<Stmt>, Option<Box<Stmt>>),
+    Print(Expr),
     Var(Token, Option<Expr>),
+    While(Expr, Box<Stmt>),
+    Block(Box<Vec<Stmt>>),
 }
 
 impl Display for Stmt {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Block(s) => write!(f, "Block for {:?}", s),
-            Self::Print(e) => write!(f, "Print {e}"),
             Self::Expression(e) => write!(f, "Expression {e}"),
             Self::If(e, tb, eb) => write!(f, "If {} for than {:?} else {:?}", e, tb, eb),
+            Self::Print(e) => write!(f, "Print {e}"),
             Self::Var(t, e) => write!(f, "Variable {t} for {:?}", e),
+            Self::While(e, s) => write!(f, "While {} for {}", e, s),
+            Self::Block(s) => write!(f, "Block for {:?}", s),
         }
     }
 }
