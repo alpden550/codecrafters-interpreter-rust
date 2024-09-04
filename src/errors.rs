e383aaff02caf967d76b2acf1ebee78beefeba25
Denis Novikov <alpden550@gmail.com>
2024-09-04 14:19:15 +0300

    feat(error): add value error enum

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,12 @@
-#[allow(dead_code)]
+use crate::models::values::Value;
+
 #[derive(Debug)]
 pub enum ExitCode {
     ExitError = 65,
     RuntimeError = 70,
+}
+
+pub enum ValueError {
+    Error(String),
+    Return(Value),
 }
