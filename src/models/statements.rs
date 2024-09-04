93686d851497bff804c248d7664eab301d28f610
Denis Novikov <alpden550@gmail.com>
2024-09-04 10:09:29 +0300

    feat(statements): add function for stmts

--- src/models/statements.rs
+++ src/models/statements.rs
@@ -5,6 +5,7 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug, Clone)]
 pub enum Stmt {
     Expression(Expr),
+    Function(Token, Vec<Token>, Box<Vec<Stmt>>),
     If(Expr, Box<Stmt>, Option<Box<Stmt>>),
     Print(Expr),
     Var(Token, Option<Expr>),
@@ -16,6 +17,9 @@ impl Display for Stmt {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Expression(e) => write!(f, "Expression {e}"),
+            Self::Function(name, params, body) => {
+                write!(f, "func {name} {:?} with body {:?}", params, body)
+            }
             Self::If(e, tb, eb) => write!(f, "If {} for than {:?} else {:?}", e, tb, eb),
             Self::Print(e) => write!(f, "Print {e}"),
             Self::Var(t, e) => write!(f, "Variable {t} for {:?}", e),
