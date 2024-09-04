fba2363e9fa2e6d5dc6c16a3599f4da0ffe05ea2
Denis Novikov <alpden550@gmail.com>
2024-09-04 10:08:46 +0300

    feat(expressions): add call for expr

--- src/models/expressions.rs
+++ src/models/expressions.rs
@@ -10,6 +10,7 @@ pub enum Expr {
     Variable(Token),
     Binary(Box<Expr>, Token, Box<Expr>),
     Assign(Token, Box<Expr>),
+    Call(Box<Expr>, Token, Box<Vec<Expr>>),
     Grouping(Box<Expr>),
 }
 
@@ -22,6 +23,9 @@ impl Display for Expr {
             Self::Variable(t) => write!(f, "(variable {t})"),
             Self::Binary(l, o, r) => write!(f, "({} {l} {r})", o.name),
             Self::Assign(t, e) => write!(f, "({} {e})", t.name),
+            Self::Call(callee, _paren, arguments) => {
+                write!(f, "func {callee} with args {:?}", arguments)
+            }
             Self::Grouping(e) => write!(f, "(group {e})"),
         }
     }
