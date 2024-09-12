64c1088924c4be53276497f7835bce6841fd0669
Denis Novikov <alpden550@gmail.com>
2024-09-12 10:27:28 +0300

    feat(errors): add display impl

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,6 @@
 use crate::models::values::Value;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub enum ExitCode {
@@ -6,7 +8,16 @@ pub enum ExitCode {
     RuntimeError = 70,
 }
 
+#[derive(Debug)]
 pub enum ValueError {
     Error(String),
     Return(Value),
 }
+
+impl Display for ValueError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for ValueError {}
